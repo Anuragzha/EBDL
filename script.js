dd7dfb06b4eb30870614640d74006c9708c9d2cd
Anurag Kumar Jha <162889558+Anuragzha@users.noreply.github.com>
2026-04-27 12:56:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// ===== SLIDER =====
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+document.querySelector(".next").onclick = () => {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+};
+
+document.querySelector(".prev").onclick = () => {
+  slides[index].classList.remove("active");
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add("active");
+};
+
+// ===== REPORT BUTTON =====
+function goToReport() {
+  alert("Report page coming soon!");
+}
